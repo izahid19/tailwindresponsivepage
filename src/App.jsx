6cd0a88df211ccf5e4af8a97a17cb4f95271ee99
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-09-09 19:03:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./Components/Header"
+import Body from "./Components/Body"
+import Footer from "./Components/Footer"
+import "./index.css"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="min-h-screen px-6 py-3 bg-amber-50">
+      <Header/>
+      <Body/>
+      <Footer/>
+
+       
+      </div>
+    </>
+  )
+}
+
+export default App
