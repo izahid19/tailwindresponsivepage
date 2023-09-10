532cdf166fe78fdcfa75c4489cd207d11a23604f
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-09-10 13:34:32 +0530

    ipad responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16">
+      <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7">
       <Header/>
       <Body/>
       <Footer/>
