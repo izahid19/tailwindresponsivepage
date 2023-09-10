e769ece45ba0c20d4e1161f0094a2e8e1257859c
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-09-10 14:16:39 +0530

    web browser responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7">
+      <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:justify-between">
       <Header/>
       <Body/>
       <Footer/>
--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
+    <div className='lg:flex'>
 
-    <div className='flex items-center justify-center'>
-      <img src="./src/assets/Blue-Shape.svg" alt="img1" className='h-64 -rotate-45 md:h-72'/>
-      <img src="./src/assets/Pink-Shape.svg" alt="img2" className='absolute h-64 -rotate-[30deg]  md:h-72'/>
-      <img src="./src/assets/Purple-shape.svg" alt="img3" className='absolute h-64 -rotate-[15deg]  md:h-72'/>
-      <img src="./src/assets/Hero-Model.png" alt="hero" className='absolute h-64  md:h-72'/>
+    <div className='flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end lg:overflow-hidden'>
+      <img src="./src/assets/Blue-Shape.svg" alt="img1" className='h-64 -rotate-45 md:h-72 lg:h-96 lg:overflow-hidden'/>
+      <img src="./src/assets/Pink-Shape.svg" alt="img2" className='absolute h-64 -rotate-[30deg]  md:h-72 lg:h-96 lg:overflow-hidden'/>
+      <img src="./src/assets/Purple-shape.svg" alt="img3" className='absolute h-64 -rotate-[15deg]  md:h-72 lg:h-96 lg:overflow-hidden'/>
+      <img src="./src/assets/Hero-Model.png" alt="hero" className='absolute h-64  md:h-72 lg:h-96 lg:overflow-hidden'/>
     </div>
-    <div className='space-y-4'>
-      <h1 className='text-5xl font-bold leading-tight font-playfair'>Host your website in less than 5 minutes</h1>
+    <div className='space-y-4 lg:flex-1 lg:order-1 lg:space-y-10'>
+      <h1 className='text-5xl font-bold leading-tight font-playfair '>Host your website in less than 5 minutes</h1>
       <p className='font-lato text-gray-400'>With Hoster , get your website up and running in no less than 5 minutes with the most competitive pricing packages available online.</p>
       <form action="" className='flex flex-col gap-4 md:flex-row'>
         <input className='rounded-md px-4 py-3 placeholder:text-gray-400' type="email" placeholder='Enter e-mail address' />
-        <button className='rounded-md px-4 py-3 bg-blue-400'>Join Waitlist</button>
+        <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join Waitlist</button>
       </form>
       <div className='flex gap-2'>
         <img src="./src/assets/Checkmark.svg" alt="" />
